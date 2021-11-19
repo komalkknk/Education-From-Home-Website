2b46bec662b81e5240af79fef81d99a7fb49799e
komalkknk <78220140+komalkknk@users.noreply.github.com>
2021-11-19 13:57:39 +0530

    Update settings.py

--- EducationFromHome/settings.py
+++ EducationFromHome/settings.py
@@ -19,11 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+$9a77ytrz7@18a9s^bx8pjh&_-531&p03*0e-7=qr8ap*@g4n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
